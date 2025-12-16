0cc3cedcffbdc847813990c8656fb56175602cbf
Vitalii <v.chernetskyi@gmail.com>
2025-12-16 20:51:41 +0200

    Fix header formatting in check_latest_news.py

--- check_latest_news.py
+++ check_latest_news.py
@@ -9,8 +9,10 @@ headers = {
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/120.0.0.0 Safari/537.36"
-    )
+    ),
+    "Accept-Language": "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7",
 }
+
 
 response = requests.get(
     URL,
